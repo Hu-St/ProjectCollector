a7fb3fb8e115c5ef66378bf6fe1aec1654630a19
hstrautz <hstrautz@intenta.de>
2014-11-17 10:50:10 +0100

    Added doxyfile and mainpage documentation header.

---
+++ source/include/pc_mainpage.h
@@ -0,0 +1,29 @@
+/**
+    \mainpage   ProjectCollector
+
+                General program informations.
+
+                Take a look at \ref site_changelog site.
+ */
+
+/**
+    \page site_changelog Changelog
+
+    \section    v_1-0 Version 1.0
+
+    \subsection v_1-0_features New Features
+
+                Nothing
+
+    \subsection v_1-0_changes Changes
+
+                Nothing
+
+    \subsection v_1-0_improvements Improvements
+
+                Nothing
+
+    \subsection v_1-0_bugfixes Bugfixes
+
+                Nothing
+ */
