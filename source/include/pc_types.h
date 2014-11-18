3396c5a4aea68a31012011c71553155408e0e0a4
hstrautz <hstrautz@intenta.de>
2014-11-18 10:44:03 +0100

    Some adaptions to doxygen documentation style.

--- source/include/pc_types.h
+++ source/include/pc_types.h
@@ -15,6 +15,7 @@
 // Includes
 //-----------------------------------------------------------------------------
 #include "pc_types_floating.hpp"
+#include "pc_types_results.hpp"
 
 //-----------------------------------------------------------------------------
 // Namespace
@@ -22,21 +23,25 @@
 namespace pc
 {
 
-    //-----------------------------------------------------------------------------
-    // Types
-    //-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+// Types
+//-----------------------------------------------------------------------------
 
-    // Unsigned integers.
-    typedef unsigned char       uint8_t;    ///< 8  bit unsigned type.
-    typedef unsigned short      uint16_t;   ///< 16 bit unsigned type.
-    typedef unsigned long       uint32_t;   ///< 32 bit unsigned type.
-    typedef unsigned long long  uint64_t;   ///< 64 bit unsigned type.
+// Character types.
+typedef char                char8_t;    ///< Signed 8 bit character type.
+typedef unsigned char       uchar8_t;   ///< Unsigned 8 bit character type.
 
-    // Signed integers.
-    typedef signed char         int8_t;     ///< 8  bit signed type.
-    typedef signed short        int16_t;    ///< 16 bit signed type.
-    typedef signed long         int32_t;    ///< 32 bit signed type.
-    typedef signed long long    int64_t;    ///< 64 bit signed type.
+// Unsigned integers.
+typedef unsigned char       uint8_t;    ///< 8  bit unsigned type.
+typedef unsigned short      uint16_t;   ///< 16 bit unsigned type.
+typedef unsigned long       uint32_t;   ///< 32 bit unsigned type.
+typedef unsigned long long  uint64_t;   ///< 64 bit unsigned type.
+
+// Signed integers.
+typedef signed char         int8_t;     ///< 8  bit signed type.
+typedef signed short        int16_t;    ///< 16 bit signed type.
+typedef signed long         int32_t;    ///< 32 bit signed type.
+typedef signed long long    int64_t;    ///< 64 bit signed type.
 
 // Floating point types are included py pc_types_floating.hpp.
 
--- source/include/pc_types_results.hpp
+++ source/include/pc_types_results.hpp
@@ -16,43 +16,43 @@
 //-----------------------------------------------------------------------------
 namespace pc
 {
-    /**
-        @brief  Type definition for result and error codes.
-     */
-    enum result_t
-    {
-        // General
-        RESULT_SUCCESS      =  0,   ///< No errors occurs duering processing.
-        RESULT_ERROR_BASE   = -1,   ///< Error base to generate further error codes.
+/**
+    \brief  Type definition for result and error codes.
+ */
+enum result_t
+{
+    // General
+    RESULT_SUCCESS      =  0,   ///< No errors occurs duering processing.
+    RESULT_ERROR_BASE   = -1,   ///< Error base to generate further error codes.
 
-        // Error codes
-        RESULT_ERROR_INVALID_ARGUMENT = RESULT_ERROR_BASE | 1
-    }; // result_t
+    // Error codes
+    RESULT_ERROR_INVALID_ARGUMENT = RESULT_ERROR_BASE | 1
+}; // result_t
 
-    //-----------------------------------------------------------------------------
-    // Helper functions
-    //-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+// Helper functions
+//-----------------------------------------------------------------------------
 
-    /**
-        @brief  Cheks if an result is successful.
-        @param[in]  result  Input result code.
-        @return Returns true if result is RESULT_SUCCESS, else true will be returned.
-     */
-    static bool IsResultSuccessful(const result_t& result)
-    {
-        return  (result == RESULT_SUCCESS) ? true : false;
-    }
+/**
+    \brief  Cheks if an result is successful.
+    \param[in]  result  Input result code.
+    \return Returns true if result is RESULT_SUCCESS, else true will be returned.
+ */
+static bool IsResultSuccessful(const result_t& result)
+{
+    return  (result == RESULT_SUCCESS) ? true : false;
+}
 
-    /**
-        @brief  Generates a desired error code if boolean expression is fulfilled.
-        @param[in]  condition   Condition that shall be fulfilled.
-        @param[in]  errorCode   Desired error code that shall be returned if condition is fulfilled.
-        @return If condition is fulfilled errorCode will be returned, else RESULT_SUCCESS will be returned.
-     */
-    static result_t GenerateErrorIf(const bool& condition, const result_t& errorCode)
-    {
-        return (true == condition) ? errorCode : RESULT_SUCCESS;
-    }
+/**
+    \brief  Generates a desired error code if boolean expression is fulfilled.
+    \param[in]  condition   Condition that shall be fulfilled.
+    \param[in]  errorCode   Desired error code that shall be returned if condition is fulfilled.
+    \return If condition is fulfilled errorCode will be returned, else RESULT_SUCCESS will be returned.
+ */
+static result_t GenerateErrorIf(const bool& condition, const result_t& errorCode)
+{
+    return (true == condition) ? errorCode : RESULT_SUCCESS;
+}
 
 //-----------------------------------------------------------------------------
 // Namespace
